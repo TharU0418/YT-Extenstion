fc7cad1f48900f0f8e62ea841b18e99c50ca6410
TharU0418 <101709936+TharU0418@users.noreply.github.com>
2023-06-17 08:41:29 +0530

    create the extension

---
+++ script.js
@@ -0,0 +1,107 @@
+const resultDis = document.getElementById("load-title");
+const resultMore = document.getElementById("result-more");
+const downloadBtn = document.getElementById("download");
+
+let video_quality;
+
+
+const dropdownSelect = document.getElementById("dropdown-select");
+
+
+chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+        
+    
+    if (tabs.length > 0) {
+        const currentUrl = tabs[0].url;
+        console.log("Current URL:", currentUrl);
+          
+        const xhr = new XMLHttpRequest();
+        
+        xhr.timeout = 150000;
+        xhr.onerror = function () {
+        resultDis.innerHTML = `
+            <h4 style="color:red">currentUrl</h4>
+        `
+        }
+    xhr.open(
+        "GET",
+        `${process.env.BACKEND_URL}/extenstion?userinput=` + currentUrl,
+        true
+    );
+
+        xhr.onload = function () {
+            if(xhr.status === 200){
+                const data = JSON.parse(xhr.responseText);
+                const title = data["Vid_title"];
+                const img = data["Thumbnail_url"];
+
+                resultDis.innerHTML = `
+                    <h5 style="color:purple; font-size:20px;">Title: ${title}</h5>
+                    <img src="${img}" style="width:400px; height:160px;"/>
+                    
+                    
+                `
+            }
+        }
+        
+        xhr.send();
+
+        }
+    });
+//});
+
+
+dropdownSelect.addEventListener("change", function() {
+    const selectedOption = dropdownSelect.value;
+    console.log("Selected option:", selectedOption);
+    video_quality = selectedOption;
+    console.log("video_quality:", video_quality);
+
+    // Perform actions based on the selected option
+    if (selectedOption === "144p") {
+       video_quality = selectedOption
+    } else if (selectedOption === "240p") {
+      video_quality = selectedOption
+    } else if (selectedOption === "360p") {
+      video_quality = selectedOption
+    }else if (selectedOption === "720p") {
+      video_quality = selectedOption
+    }else if (selectedOption === "1080p") {
+      video_quality = selectedOption
+    }
+
+    video_quality = selectedOption;
+    console.log("video_quality:", video_quality);
+    
+  });
+  console.log('2',video_quality)
+
+
+downloadBtn.addEventListener("click", function() {
+
+    
+
+chrome.tabs.query({active:true, currentWindow: true}, function(tabs){
+        
+    
+    if (tabs.length > 0) {
+        const currentUrl = tabs[0].url;
+ 
+        const xhr = new XMLHttpRequest();
+
+        console.log("vq",video_quality)
+
+    xhr.open(
+        "GET",
+        `${process.env.BACKEND_URL}?userinput=${currentUrl}&video_quality=${video_quality}` , 
+        true
+    );
+
+        
+        xhr.send();
+
+        }
+    });
+});
+
+
